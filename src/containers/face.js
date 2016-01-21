43a5a2f5ceb7ae87ac3db9bb90d38f25306880c5
sqrtofsaturn <sqrt@octoblu.com>
2016-01-21 16:22:21 -0700

    get basic stuff working

--- src/containers/face.js
+++ src/containers/face.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import ClassNames from 'classnames'
 
-import Loading from '../components/loading'
+var meshbluConfig = {
+  "uuid": "cbb0ae28-965a-49bd-b6b0-a30a2eda5094",
+  "token": "d1d83f51d97ebcf4825ebb7bbf8ef8d77fdcca51",
+  "server": "meshblu.octoblu.com",
+  "port": 443
+}
 
-export default class Face extends Component {
+var conn = meshblu.createConnection(meshbluConfig)
+
+
+// var meshblu = new MeshbluWebsocket(meshbluConfig);
+console.log(meshbluConfig)
+
+class Face extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  state = {
+    face: false
+  }
+
+  componentDidMount() {
+    var self = this
+    conn.on('ready', function(data){
+      self.setState({face: true})
+    });
+
+    conn.on('message', function(message){
+      // self.setState({face: true})
+    });
+    // LISTEN a
+
+  }
   render() {
+    console.log(this.state);
+    let componentClass = ClassNames( { 'face': this.state.face})
     return <div>
-      <h2>Face</h2>
+      <div className={componentClass}>{this.state.face}</div>
     </div>
   }
 }
+
+export default Face
