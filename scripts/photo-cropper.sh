11f32fd4c445c069d7d1bee80261889cf0dfd8cc
sqrtofsaturn <sqrt@octoblu.com>
2016-01-21 17:37:35 -0700

    trimmed chris head

--- scripts/photo-cropper.sh
+++ scripts/photo-cropper.sh
@@ -1,5 +1,2 @@
-for file in *; echo "convert $file -crop 1100x1100+900+1100 $file-cropped.jpg"; end
-
-
-
-convert IMG_20160121_164316.jpg -crop 900x900+1000+1200 cropped/cropped.jpg
+#crop faces
+for file in *; convert $file -crop 900x900+1000+1200 cropped/$file; end
