cfc5bb8dc06224106047fa600e1e3825e292c414
sqrtofsaturn <sqrt@octoblu.com>
2016-01-21 18:16:45 -0700

    8-bit lightened chris!

--- scripts/photo-cropper.sh
+++ scripts/photo-cropper.sh
@@ -1,2 +1,12 @@
 #crop faces
 for file in *; convert $file -crop 900x900+1000+1200 cropped/$file; end
+
+#trim whitespace
+for file in *; convert $file -fuzz 40% -trim trimmed/$file; end
+
+
+#clear background
+for file in *; convert $file -fuzz 11% -transparent "rgb(172,170,158)" transparent/$file; end
+
+#8-bit
+for file in *; convert $file -depth 3 PNG8:contrast.png 8-bit/$file; end
