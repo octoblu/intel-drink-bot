5db328d447ad566f78c8c3f18546512af4730e05
redaphid <a@octoblu.com>
2016-01-23 17:45:44 -0700

    v1.0.17 marquee

--- src/containers/marquee.js
+++ src/containers/marquee.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react'
 
 import Loading from '../components/loading'
 import OrderList from '../components/orders/order-list'
+import ClassNames from 'classnames'
 
 export default class Marquee extends Component {
-
+  constructor(props) {
+    super(props)
+      _.bindAll(this, [
+        'render',
+        'setText',
+        'toggleEdit'
+      ])
+  }
   state = {
     text: 'loading'
   }
 
   componentDidMount() {
-    this.setState({text: 'hi'})
+    this.setState({text: '', showEdit: true})
   }
 
   render() {
+    let inputClasses = ClassNames({hide: !this.state.showEdit})
 
     return <div id="marquee">
-        <input onKeyDown={self.setText}/>
-        <h1><marquee>{this.state.text}</marquee></h1>
+        <header className="toolbar">
+          <input className={inputClasses} onChange={this.setText} value={this.state.text}/>
+        </header>
+        <marquee onClick={this.toggleEdit}>{this.state.text || 'Click Me to Edit'}</marquee>
       </div>
+  }
+
+  setText(event) {
+    this.setState({text: event.target.value})
+  }
+
+  toggleEdit(event) {
+    event.preventDefault()
+    this.setState({showEdit: !this.state.showEdit})
+    console.log('showEdit', this.state.showEdit)
   }
 
 }
